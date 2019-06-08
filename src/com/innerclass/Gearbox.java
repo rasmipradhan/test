0043cf2edd2903b4270a65ee16805b255211e91c
pradhan <pradhan@gmail.com>
2019-06-08 12:59:28 -0400

    demo

---
+++ src/com/innerclass/Gearbox.java
@@ -0,0 +1,68 @@
+package com.innerclass;
+
+import java.util.ArrayList;
+
+public class Gearbox {
+	private ArrayList<Gear> gears ;
+	private int maxGears;
+	private int currentGear = 0;
+	private boolean clutchIsIn; 
+	
+	public Gearbox(int maxGears){
+		this.gears = new ArrayList<>();
+		this.maxGears = maxGears;
+		Gear neutral = new Gear(0, 0.0);
+		this.gears.add(neutral);
+	}
+	
+	public void operateClutch(boolean in){
+		this.clutchIsIn = in;
+	}
+	
+	public void addGear(int number, double ratio){
+		if((number > 0) && (number <= this.maxGears)){
+			this.gears.add(new Gear(number, ratio));
+		}
+	}
+	
+	public void changeGear(int newGear){
+		if((newGear >= 0) && (newGear < this.gears.size()) && this.clutchIsIn){
+			this.currentGear = newGear;
+			System.out.println("Gear " + newGear + " selected.");
+		}else {
+			System.out.println("Grind!");
+			this.currentGear = 0;
+		}	
+		
+	}
+	
+    public double wheelSpeed(int revs) {
+        if(clutchIsIn) {
+            System.out.println("Scream!!!");
+            return 0.0;
+        }
+        return revs * gears.get(currentGear).getRatio();
+    }
+
+	
+	private class Gear{
+		private int grearNumber;
+		private double ratio;
+		
+		
+		public Gear(int grearNumber, double ratio) {
+			this.grearNumber = grearNumber;
+			this.ratio = ratio;
+		}
+
+
+		public double getRatio() {
+			return ratio;
+		}
+		
+		public double driveSpeed(int revs){
+			return revs * this.ratio;
+		}
+		
+	}
+}
